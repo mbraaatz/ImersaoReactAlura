3d01835a58e053d6f4b63198752dbad46a040373
Michele Braatz <mbraaatz@gmail.com>
2020-08-10 10:28:23 -0300

    Atualizando pages/cadastro/Categoria/index

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,20 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../Components/PageDefault';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
-      <form>
-        <label>
-          Nome da Categoria:
-          <input type="text" />
-        </label>
+      <form onSubmit={function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          nomeDaCategoria
+        ]);
+        
+      }}>
+        
+        <div>
+          <label>
+            Nome da Categoria:
+            <input type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandlerQueOErroPediu(infosDoEvento){
+              setNomeDaCategoria(infosDoEvento.target.value);
+            }}
+            />
+          </label>
+        </div>
+
+        <div>    
+          <label>
+            Descrição:
+            <input type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandlerQueOErroPediu(infosDoEvento){
+              setNomeDaCategoria(infosDoEvento.target.value);              
+            }}
+            />
+          </label>
+        </div>
+
+        <div>    
+          <label>
+          Cor:
+          <input type="color"
+          value={nomeDaCategoria}
+          onChange={function funcaoHandlerQueOErroPediu(infosDoEvento){
+            setNomeDaCategoria(infosDoEvento.target.value);
+          }}
+          />
+          </label>  
+        </div>
         <button>
           Cadastrar
         </button>
       </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
+
+      {/* <Link to="/">
+        Ir para home
+      </Link> */}
     </PageDefault>
   )
 }
